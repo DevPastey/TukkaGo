b0354a8544a5f8b06d089696de49ee72ebb29149
DevPastey <emmanuelolabodee@outlook.com>
2025-11-16 01:44:38 +0100

    Implement sign-in tab UI

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,26 +1,64 @@
 import { images } from '@/constants';
-import { Slot } from 'expo-router';
+import { router, Slot } from 'expo-router';
+import { useState } from 'react';
+import cn from "clsx";
 
-import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { set } from 'mongoose';
 
 export default function _Layout() {
+  const [activeTab, setActiveTab] = useState<"sign-in" | "sign-up">("sign-in");
 
+
+
+  const handlePress = (id: "sign-in" | "sign-up") => {
+    setActiveTab(id);
+    router.push(`/${id}`)
+  };
+
+  
+  
 
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <ScrollView className='bg-white h-full' keyboardShouldPersistTaps="handled">
         <View className="w-full relative" 
           style={{
-            height: Dimensions.get("screen").height/2.25,
+            height: Dimensions.get("screen").height/2.5,
           }}
         >
           <ImageBackground source={images.loginGraphic} className='rounded-b-lg size-full' resizeMode='stretch' />
-          <Image source={images.logo} className='self-center size-28 rounded-full -bottom-10 absolute z-10'/>
+         
+          <View className='self-center size-20 flex justify-center items-center rounded-full top-[18rem] absolute z-40 bg-white'>
+            <Image source={images.logo} className='self-center size-20'/>
+          </View>
+          <View style={styles.overlay} className='z-10 rounded-b-3xl' />
+
         </View>
-        <Slot />
+        
+
+        <View className='flex flex-row mt-8 justify-center items-center bg-white-100 mx-6 py-1 px-1 rounded'>
+          <TouchableOpacity className={cn('justify-center items-center bg-white py-4 w-[50%] rounded', activeTab === "sign-in" ? "bg-white" : "bg-white-100")} onPress={() => handlePress('sign-in')}>
+            <Text className={cn("font-semibold", activeTab === "sign-in" ? "text-primary" : "text-gray-100")}>Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className={cn('justify-center items-center bg-white flex w-[50%] py-4 rounded', activeTab === "sign-up" ? "bg-white" : "bg-white-100")} id='sign-up' onPress={() => handlePress('sign-up')} > 
+          <Text className={cn("font-semibold" , activeTab === "sign-up"  ? "text-primary" : "text-gray-100")} >Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+        <Slot  />
       </ScrollView>
       
     </KeyboardAvoidingView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  overlay: {
+    ...StyleSheet.absoluteFillObject, // fills the parent
+    backgroundColor: "rgba(0,0,0,0.1)", // black with 50% opacity
+    height: Dimensions.get("screen").height/2.75,
+  },
+
+});
